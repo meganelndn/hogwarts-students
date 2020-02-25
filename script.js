e926ee03ec5a42034bd817080c898bd2c2521416
megane <megane.londono@hotmail.com>
2020-02-25 15:14:01 +0100

    student photos and crests appear in modal

--- script.js
+++ script.js
@@ -1,84 +1,154 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", init);
 
-const HTML = {};
-
+/*------------------STUDENT ARRAY--------------------*/
 let studentArr = [];
-
-const studentObj = {
+let nameArr = {};
+/*-----------------OBJECT PROTOTYPE------------------*/
+const studentObject = {
   firstName: "",
   lastName: "",
   middleName: "",
   nickName: "",
   image: "",
   house: ""
-}
+};
 
 function init() {
   console.log("ready");
-  HTML.modal = document.querySelector(".modal-background");
-  HTML.close = document.querySelector(".close");
-  HTML.wrapper = document.querySelector("#students");
+
+  // listen for theme selection
+  document.querySelector("select#theme").addEventListener("change", function () {
+    document.querySelector("body").setAttribute("data-house", this.value);
+  });
 
   fetchJson();
 }
+/*-------------------SORT-------------------*/
+/*------------------FILTER------------------*/
 
 function fetchJson() {
   fetch("https://petlatkea.dk/2020/hogwarts/students.json")
     .then(res => res.json())
-    .then(handleData);
+    .then(getStudents);
 }
 
-function selectTheme() {
-  document.querySelector("body").setAttribute("data-house", this.value);
+/*-------------------DISPLAY STUDENTLIST--------------------*/
+function getStudents(studentArr) {
+  studentArr.forEach(separateNames);
+  //console.log(studentArr);
 }
 
-function handleData(studentArr) {
-  console.log(studentArr);
-  // loop array
-  studentArr.forEach(showStudents);
-  // listen for event change
-  document.querySelector("select#theme").addEventListener("change", selectTheme);
+/*---------------------CLEAN JSON DATA-----------------------*/
+function separateNames(student) {
+  // Create new object from prototype
+  let newStudent = Object.create(studentObject);
+  //console.log(student) // logs each student object
+
+  // full name
+  let fullName = student.fullname;
+
+  // split full name into parts
+  nameArr = fullName.split(" ");
+
+  nameArr.forEach(generalCleanUp);
+  console.log(nameArr)
+
+  // first name
+  newStudent.firstName = nameArr[0];
+
+  // middle name & nick name
+  if (nameArr.length > 2) {
+    newStudent.middleName = nameArr.slice(1, nameArr.length - 1);
+    newStudent.nickName = nameArr.slice(1, nameArr.length - 1);
+  }
+
+  // last name
+  newStudent.lastName = nameArr[nameArr.length - 1];
+
+  // image filename
+  newStudent.image = newStudent.lastName.toLowerCase() + "_" + newStudent.firstName[0].substring(0, 1).toLowerCase() + ".png";
+
+  // house
+  newStudent.house = student.house.toLowerCase();
+
+  studentArr.push(newStudent);
+  console.log(studentArr)
+
+  showStudents(newStudent);
 }
 
-
-/********************* CLEAN JSON DATA ******************/
-
-function cleanData() {
-
-  // TODO: insert clean data here
-
-  showStudents(student);
-}
-
-function showStudents(student) {
-  // clone template
+/*-------------------DISPLAY STUDENTLIST------------------*/
+function showStudents(newStudent) {
+  // clone HTML template
   const template = document.querySelector("#template").content;
   const copy = template.cloneNode(true);
 
-  // populate template with content being fetched
-  copy.querySelector(".studentName").textContent = student.fullname;
-
-  // textContent in modal
+  copy.querySelector(".studentFirstName", ".studentLastName").textContent = newStudent.firstName + " " + newStudent.lastName;
   copy.querySelector(".modalBtn").addEventListener("click", function () {
+
+    // match color and crest with student in modal
+    const modal = document.querySelector(".modal-content");
+    modal.setAttribute("data-house", newStudent.house);
+    // TODO: add blood status
+
+    // TODO: add if prefect, expelled, or member of inquisitorial squad
+
+    // TODO: add student image
+    modal.querySelector(".photo").src = `images/${newStudent.lastName.toLowerCase() + "_" + newStudent.firstName[0].substring(0, 1).toLowerCase() + ".png"}`;
+    // add house crest
+    modal.querySelector(".crest").src = `house-crests/${newStudent.house.toLowerCase()}.png`;
+
+
     const modalOpen = document.querySelector(".modal-background");
     modalOpen.classList.remove("hide");
     const modalHouse = document.querySelector(".modal-house");
-    modalHouse.textContent = student.fullname;
+    modalHouse.textContent = newStudent.house;
     const modalName = document.querySelector(".modal-name");
-    modalName.textContent = student.house;
+    modalName.textContent = newStudent.firstName + " " + newStudent.lastName;
   });
 
-  // append copy to parent element
+  // append template copy
   document.querySelector("#students").appendChild(copy);
 
   closeModal();
 }
 
 function closeModal() {
-  // user can now click anywhere to close the modal
   const modalClose = document.querySelector(".modal-background");
   modalClose.addEventListener("click", () => {
     modalClose.classList.add("hide");
   });
+}
+
+/*-------------------DISPLAY CLEANED UP STUDENTLIST------------------*/
+function generalCleanUp(fullName, index) {
+  // if index 0 = empty, delete from beginning of array
+  if (nameArr[0] == "") {
+    nameArr.shift();
+  }
+
+  // if (nameArr.length -1) = empty, delete from end of array
+  if (nameArr[nameArr.length - 1] == "") {
+    nameArr.pop();
+  }
+
+  /*--------------OBJECT TO ARRAY----------------*/
+  let newArr = Array.from(nameArr[index]);
+
+  //delete or change character
+  newArr.forEach((character, index) => {
+    if (character == '"' || character == `"\"`) {
+      newArr[index] = "";
+    } else if (index == 0) {
+      newArr[0] = newArr[0].toUpperCase();
+    } else {
+      newArr[index] = newArr[index].toLowerCase();
+    }
+  });
+
+  // update that string in the fullNameArray after changing each character
+  let newStr = newArr.join("");
+  nameArr[index] = newStr;
+  console.log(newStr)
 }
